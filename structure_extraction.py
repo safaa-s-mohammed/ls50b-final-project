23e4aad59661f6a3a9b475cdc390eda2b3a402b3
safaa <safaamohammed@college.harvard.edu>
2026-04-21 22:50:17 -0400

    finished all joao's pdbs

--- structure_extraction.py
+++ structure_extraction.py
@@ -12,7 +12,7 @@ from pymol import cmd
 from itertools import product
 
 # Configuration, variable initialization
-pdb_file = r"C:\Users\safaa\Downloads\4OLW.pdb"
+pdb_file = r"C:\Users\safaa\Downloads\4OM1.pdb"
 antibody_chains = ["H", "L"] # Creates sections for both heavy and light chains
 antigen_chains = ["G"]
 output_distance_file = "distance_matrix.csv"
